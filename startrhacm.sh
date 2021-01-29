a04eead54b55d2d352689ddccbbbf59f61776bda
Dale Haiducek <dhaiduce@redhat.com>
2021-01-29 14:39:24 -0500

    Minor bug fixes

--- startrhacm.sh
+++ startrhacm.sh
@@ -20,7 +20,7 @@ function printlog() {
 
 # Helper function to check exports
 function checkexports() {
-  if [[ -z ${LIFEGUARD_PATH} ]]; then
+  if [[ -z "${LIFEGUARD_PATH}" ]]; then
     printlog error "LIFEGUARD_PATH not defined. Please set LIFEGUARD_PATH to the local path of the Lifeguard repo."
     exit 1
   else
@@ -29,7 +29,7 @@ function checkexports() {
       exit 1
     fi
   fi
-  if [[ -z ${RHACM_PIPELINE_PATH} ]]; then
+  if [[ -z "${RHACM_PIPELINE_PATH}" ]]; then
     printlog error "RHACM_PIPELINE_PATH not defined. Please set RHACM_PIPELINE_PATH to the local path of the Pipeline repo."
     exit 1
   else
@@ -38,7 +38,7 @@ function checkexports() {
       exit 1
     fi
   fi
-  if [[ -z ${RHACM_DEPLOY_PATH} ]]; then
+  if [[ -z "${RHACM_DEPLOY_PATH}" ]]; then
     printlog error "RHACM_DEPLOY_PATH not defined. Please set RHACM_DEPLOY_PATH to the local path of the Deploy repo."
     exit 1
   else
@@ -69,7 +69,7 @@ fi
 CLUSTER=$(oc config get-contexts | awk '/^\052/ {print $3}' | awk '{gsub("^api-",""); gsub("(\/|-red-chesterfield).*",""); print}')
 if [[ "${CLUSTER}" != "collective-aws" ]] || (! oc status &>/dev/null); then
   printlog info "The oc CLI is not currently logged in to the collective cluster. Please configure the CLI and try again."
-  printlog info "KUBECONFIG is currently set: $(test -n ${KUBECONFIG} && echo "true" || echo "false")"
+  printlog info "KUBECONFIG is currently set: $(test -n "${KUBECONFIG}" && echo "true" || echo "false")"
   printlog info "Current cluster: ${CLUSTER}"
   printlog info "Link to Collective cluster login command: https://oauth-openshift.apps.collective.aws.red-chesterfield.com/oauth/token/request"
   exit 1
@@ -83,13 +83,13 @@ printlog info "Switching to main branch and updating repo (if this exits, check 
 git checkout main &>/dev/null
 git pull &>/dev/null
 # Set lifetime of claim to end of work day
-if [[ -n ${CLUSTERCLAIM_END_TIME} ]]; then
+if [[ -n "${CLUSTERCLAIM_END_TIME}" ]]; then
   export CLUSTERCLAIM_LIFETIME="$((${CLUSTERCLAIM_END_TIME}-$(date "+%-H")-1))h$((60-$(date "+%-M")))m"
 fi
 ./apply.sh
 printlog title "Setting KUBECONFIG and checking cluster access"
 # If we have a ClusterClaim name, use that to get the kubeconfig, otherwise just get the most recently modified (which is most likely the one we need)
-if [[ -n ${CLUSTERCLAIM_NAME} ]]; then
+if [[ -n "${CLUSTERCLAIM_NAME}" ]]; then
   export KUBECONFIG=$(ls ${CLAIM_DIR}/${CLUSTERCLAIM_NAME}/kubeconfig)
 else
   export KUBECONFIG=$(ls -dt1 ${CLAIM_DIR}/*/kubeconfig | head -n 1)
@@ -116,7 +116,7 @@ BRANCH=${RHACM_BRANCH:-$(git remote show origin | grep -o " [0-9]\+\.[0-9]\+-" |
 if [[ ${DOWNSTREAM} == "true" ]]; then
   printlog info "Getting downstream snapshot"
   RHACM_SNAPSHOT=$(curl -s https://quay.io/api/v1/repository/acm-d/acm-custom-registry/tag/ | jq -r '.tags[].name' | grep -v "nonesuch\|-$" | grep -F "${RHACM_VERSION}" | grep -F "${BRANCH}."| head -n 1)
-  if [[ -z ${RHACM_SNAPSHOT} ]]; then
+  if [[ -z "${RHACM_SNAPSHOT}" ]]; then
     printlog error "Error querying snapshot list--nothing was returned. Please check https://quay.io/api/v1/repository/acm-d/acm-custom-registry/tag/, your network connection, and any conflicts in your exports:"
     printlog error "Query used: RHACM_VERSION: '${RHACM_VERSION}' RHACM_BRANCH '${RHACM_BRANCH}'"
     exit 1
@@ -134,7 +134,7 @@ else
   MANIFEST_TAG=$(ls ${RHACM_PIPELINE_PATH}/snapshots/manifest-* | grep -F "${VERSION_NUM}" | tail -n 1 | grep -o "[[:digit:]]\{4\}\(-[[:digit:]]\{2\}\)\{5\}.*")
   SNAPSHOT_TAG=$(echo ${MANIFEST_TAG} | grep -o "[[:digit:]]\{4\}\(-[[:digit:]]\{2\}\)\{5\}")
   VERSION_NUM=$(echo ${MANIFEST_TAG} | grep -o "\([[:digit:]]\+\.\)\{2\}[[:digit:]]\+")
-  if [[ -n ${RHACM_VERSION} && "${RHACM_VERSION}" != "${VERSION_NUM}" ]]; then
+  if [[ -n "${RHACM_VERSION}" && "${RHACM_VERSION}" != "${VERSION_NUM}" ]]; then
     printlog error "There's an unexpected mismatch between the version provided, ${RHACM_VERSION}, and the version found, ${VERSION_NUM}. Please double check the Pipeline repo before continuing."
     exit 1
   fi
@@ -164,7 +164,7 @@ if [[ "${DOWNSTREAM}" == "true" ]]; then
   oc set data secret/pull-secret -n openshift-config --from-literal=.dockerconfigjson="$(jq -s '.[0] * .[1]' <<<${FULL_TOKEN})"
   printlog info "Applying downstream resources (including ImageContentSourcePolicy to point to downstream repo)"
   oc apply -k ${RHACM_DEPLOY_PATH}/addons/downstream
-  printlog info "Waiting for up to 10 minutes for cluster nodes to update with ImageContentSourcePolicy change"
+  printlog info "Waiting up to 10 minutes for cluster nodes to update with ImageContentSourcePolicy change"
   READY="false"
   ATTEMPTS=0
   MAX_ATTEMPTS=10
@@ -186,7 +186,7 @@ printlog title "Setting oc CLI context to open-cluster-management namespace"
 oc config set-context --current --namespace=open-cluster-management
 
 # Configure auth to allow requests from localhost
-if [[ -n ${AUTH_REDIRECT_PATHS} ]]; then
+if [[ -n "${AUTH_REDIRECT_PATHS}" ]]; then
   printlog title "Waiting for ingress to be running to configure localhost connections"
   ATTEMPTS=0
   MAX_ATTEMPTS=15
